1c5f0d72bfe5428c7d2614c0a3fb99e245d3c9ad
遥遥领先 <lanny.qin@qq.com>
2025-11-20 19:27:52 +0800

    Revert "可在更改设置后选择是否自动重新启动"
    This reverts commit c0e278e613a11e0019c0e7f105943f5b151f252e.

--- MSFSMouseYoke/SettingsForm.cs
+++ MSFSMouseYoke/SettingsForm.cs
@@ -82,16 +82,9 @@ namespace MSFSMouseYoke
         {
             // 保存到配置文件
             SaveSettings();
-            DialogResult result = MessageBox.Show("设置已保存，下次启动时生效。\n是否立即重新启动", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
-            if (result == DialogResult.Yes)
-            {
-                Application.Restart();
-            }
-            else
-            {
-                this.DialogResult = DialogResult.OK;
-                this.Close();
-            }
+            MessageBox.Show("设置已保存，下次启动时生效。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.DialogResult = DialogResult.OK;
+            this.Close();
         }
 
         private void SaveSettings()
